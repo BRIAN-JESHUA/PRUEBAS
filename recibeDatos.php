ffa4ba69e562f977fd17db07b4035a5e74ac1ce8
BRIAN-JESHUA <65926757+BRIAN-JESHUA@users.noreply.github.com>
2022-06-10 00:38:55 -0300

    Add files via upload

--- recibeDatos.php
+++ recibeDatos.php
@@ -1,8 +1,19 @@
 <?php
-$nombre=$_POST['JUEGO'];
-$apellidos=$_POST['ID'];
-$sexo=$_POST['REGION'];
-echo "JUEGO: $nombre </br>";
-echo "ID: $apellidos </br>";
-echo "REGION: $sexo </br>";
+$nombre=$_POST['nombre'];
+$apellidos=$_POST['apellidos'];
+$edad=$_POST['edad'];
+
+$sexo=$_POST['sexo'];
+$gEstudios=$_POST['gEstudios'];
+$usuario=$_POST['usuario'];
+$contrasena=$_POST['contrasena'];
+$confirma=$_POST['confirma'];
+echo "El nombre es: $nombre </br>";
+echo "El apellido es: $apellidos </br>";
+echo "La edad es: $edad </br>";
+echo "Sexo es: $sexo </br>";
+echo "El grado de estudios es: $gEstudios </br>";
+echo "El usuario es: $usuario </br>";
+echo "La contraseña es: $contrasena </br>";
+echo "La confirmacion es: $confirma </br>";
 ?>
